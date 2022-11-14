8da5ac3f142f0ac5c4e9cc2bed5737c48c9f86d3
hiroki0116 <hirokiseino0116@gmail.com>
2022-11-15 01:05:27 +1100

    First Commit

---
+++ cmd/api/movie-handlers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		app.logger.Print(errors.New("invalid id parameter"))
+		app.errorJSON(w, err)
+		return
+	}
+
+	movie, err := app.models.DB.Get(id)
+
+	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+	if err != nil {
+		app.logger.Print(err)
+	}
+
+}
+
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+
+}
